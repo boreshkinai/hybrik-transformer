6911f29634a60ec3a857ddedd477e531e60f88d2
Boris Oreshkin <boris.oreshkin@gmail.com>
2023-02-12 16:57:02 -0500

    Delete download_pw3d_annotations.sh

--- scripts/download_pw3d_annotations.sh
+++
@@ -1,8 +0,0 @@
-PROJECT_PATH=/workspace/hybrik-transformer
-HYBRIDIK_PATH=${PROJECT_PATH}/HybrIK
-
-
-mkdir -p ${HYBRIDIK_PATH}/data/pw3d/json && \
-    cd ${HYBRIDIK_PATH}/data/pw3d/json && \
-    gdown https://drive.google.com/drive/folders/1f7DyxyvlC9z6SFT37eS6TTQiUOXVR9rK -O ${HYBRIDIK_PATH}/data/pw3d/json --folder
-    
