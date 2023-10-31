b19c35ba92dd62061e0892033ff0760a1ef36028
Boris Oreshkin <boris.oreshkin@unity3d.com>
2023-10-31 09:12:21 -0400

    Clean up and save

--- HybrIK/scripts/train_smpl.sh
+++ HybrIK/scripts/train_smpl.sh
@@ -13,8 +13,12 @@ python ./train_smpl.py \
     --exp-id ${EXPID} \
     --cfg ${CONFIG} --seed 123123 > 'tensorboard/'${CONFIG}'-'${EXPID}'/log.log'  2>&1 &
     
-
-
+# ./scripts/train_smpl.sh hybrik_transformer_warmup_4000_hrnet_bs56_lr0.0005_w_pw3d ./configs/hybrik_transformer_smpl24_hrnet_w_pw3d.yaml
+    
+# ./scripts/train_smpl.sh hybrik_transformer_warmup_4000_hrnet_bs56_lr0.0005 ./configs/hybrik_transformer_smpl24_hrnet.yaml
+    
+# ./scripts/train_smpl.sh hybrik_transformer_warmup_6000_batch_128_lr0.00025 ./configs/hybrik_transformer_smpl24.yaml
+# ./scripts/train_smpl.sh hybrik_transformer_warmup_6000 ./configs/hybrik_transformer_smpl24.yaml
 # ./scripts/train_smpl.sh hybrik_transformer_blocks6_layers3_lw512_dp0.1_inv_sqrt_lr0.0005 ./configs/hybrik_transformer_smpl24.yaml
 
 #  ./scripts/train_smpl.sh hybrik ./configs/256x192_adam_lr1e-3-res34_smpl_24_3d_base_2x_mix.yaml
